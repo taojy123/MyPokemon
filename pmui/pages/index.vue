d2ec70f52853c622f860433a8e2dd0f82029d810
taojy123 <taojy123@163.com>
2019-09-11 15:37:30 +0800

    improve

---
+++ pmui/pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <mt-header fixed title="我的对局">
+      <mt-button slot="right" icon="more" @click="newGame"></mt-button>
+    </mt-header>
+
+    <div class="games">
+      <mt-cell v-for="game in games" :key="game.id" :value="game.name" :is-link="true" :to="`/game/${game.id}`">
+        <span slot="icon">
+          <img v-for="pic in game.pics" :key="pic" width="24" height="24" :src="pic">
+        </span>
+      </mt-cell>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  head: { title: '我的对局' },
+  data() {
+    return {
+      name: '123',
+      games: []
+    }
+  },
+  methods: {
+    newGame () {
+      this.$router.push('/new')
+      // top.location.href = '/new'
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ pmui/pages/new.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <mt-header fixed title="新建对局">
+    </mt-header>
+
+    <div>
+      <mt-field label="名称" placeholder="请输入名称" v-model="name"></mt-field>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  head: { title: '新建对局' },
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    newGame () {
+      this.$router.push('/new')
+      // top.location.href = '/new'
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
